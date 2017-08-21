2e8c3bb2115dbe2ef5c1740169e1d831c36956b8
Jonathan Ganc <jonganc@gmail.com>
2017-08-21 00:23:53 -0400

    Use .eslintrc.js because it gives slightly more flexibility

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+      }
+    ],
+    'no-use-before-define': [
+      'error', { functions: false, classes: true, variables: true }],
+    'max-len': ['warn', 80],
+    'no-console': ['off'],
+    'no-underscore-dangle': ['warn', {allow: ['_id']}],
+    'no-param-reassign': ['error', { props: false }],
+    'no-unused-vars': [
+      'warn', {args: 'none', varsIgnorePattern: '^_'}],
+    'quote-props' : ['warn', 'consistent-as-needed'],
+    'no-else-return': ['off']
+  }
+};
