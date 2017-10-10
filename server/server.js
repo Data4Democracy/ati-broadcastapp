3fb4b430310f56e06e183cb58c0aa1208a903225
Jonathan Ganc <jonganc@gmail.com>
2017-10-10 00:32:35 -0400

    Set up index-wrapper for taking care of login logic

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+//  Code to start up the server
+
 //  Commented items may be of later use
 
 import express from 'express';
@@ -8,20 +10,25 @@ import * as gcpDebugAgent from '@google-cloud/debug-agent';
 //  Some modules we may later need:
 // import favicon from 'serve-favicon';
 // import logger from 'morgan';
-// import cookieParser from 'cookie-parser';
-// import session from 'express-session';
-// import connectMongo from 'connect-mongo';
 
 import getConfigPromise from './config';
-import initDb from './models/db';
-import index from './routes/index';
+import getApiRouter from './routes/index-wrapper';
 
 SourceMapSupport.install();
 gcpDebugAgent.start();
 
-(async function main() {
-  // const MongoStore = connectMongo(session);
+async function main() {
+  const config = await getConfigPromise();
   const app = express();
+
+  //  are we in production?
+  app.locals.isProduction = !(app.get('env') === 'development');
+
+  if (app.locals.isProduction) {
+    console.error(
+      'Warning: app is being run in the development environment.'
+        + ' This can open security holes if used in production.');
+  }
 
   // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   // app.use(logger('dev'));
@@ -29,34 +36,25 @@ gcpDebugAgent.start();
   const staticDir = path.join(__dirname, '..', 'static');
 
   app.use(express.static(staticDir));
-  // app.use(bodyParser.urlencoded({ extended: false }));
-  // app.use(cookieParser());
-  // app.use(session({
-  //   secret: credentials.cookieSecret,
-  //   store: new MongoStore({ mongooseConnection: mongoose.connection })
-  // }));
 
-  //  add a atiba object to req, res for custom attributes
-  app.use((req, res, next) => {
-    req.atiba = {};
-    res.atiba = {};
-    next();
-  });
+  // trust proxy when it says, e.g., that pre-forwarded request used https
+  if (config.get('is_gae')) app.set('trust proxy', 1);
 
-  app.use('/api', index);
+  app.use('/api', await getApiRouter());
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(staticDir, 'index.html'));
   });
 
-  //  we need the error handling for the async stuff
+  const port = config.get('port');
+  app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+  });
+}
+
+(async function mainWrapper() {
   try {
-    await initDb();
-    const config = await getConfigPromise();
-    const port = config.get('port');
-    app.listen(port, () => {
-      console.log(`App started on port ${port}`);
-    });
+    await main();
   } catch (err) {
     console.log('ERROR:', err);
   }
